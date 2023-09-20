6c4312cc8509514c52618fb72462a482a9c5b29e
Abel Fernandez <abelfernandez550@gmail.com>
2023-09-20 14:48:32 -0300

    Practica 8 finalizada, con correcciones basadas en las devoluciones

---
+++ Lab.Net.EF/LabNetPracticaNro8/src/app/interfaces/empleadoView.ts
@@ -0,0 +1,12 @@
+
+
+export interface Empleado {
+    Id?:number;
+    Nombre: string;
+    Apellido: string;
+    Direccion?: string;
+    Ciudad?: string;
+    Pais?: string;
+    FechaContratacion?: Date;
+
+  }
